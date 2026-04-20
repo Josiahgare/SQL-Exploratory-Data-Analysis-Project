cd2a3be989c5794278f6011e5b15acd697b15da8
Josiah Obarhuakpor <josiahgare@gmail.com>
2026-04-20 12:08:50 -0500

    Create 02_dimensions_exploration.sql

---
+++ scripts/02_dimensions_exploration.sql
@@ -0,0 +1,14 @@
+-- ==============================
+-- 2.	Dimension Exploration
+-- ==============================
+-- Identifying the unique values (or categories) in each dimension. Helps to recognze howthe data might be 
+-- grouped or segmented,which is useful for later analysis. 
+-- Formula: DISTINCT[dimension]
+
+-- Explore all the Countries our customers come from
+SELECT DISTINCT country FROM gold.dim_customers;
+
+-- Explore all the Categories "The Major Divisions"
+SELECT DISTINCT category, subcategory, product_name 
+FROM gold.dim_products
+ORDER BY 1,2,3;
