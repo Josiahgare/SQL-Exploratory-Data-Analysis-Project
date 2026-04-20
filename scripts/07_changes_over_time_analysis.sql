1f86b2d418b86d9b4a2ee25d5c375243beaca493
Josiah Obarhuakpor <josiahgare@gmail.com>
2026-04-20 12:31:30 -0500

    Create 07_changes_over_time_analysis.sql

---
+++ scripts/07_changes_over_time_analysis.sql
@@ -0,0 +1,38 @@
+/*
+=====================================================
+Changes Over Time Analysis
+=====================================================
+Technique to analyze how a measure evolves over time
+It helps track trends and identify seasonality in the data
+Formula: Agg[Measure] By [Date Dimension]
+=====================================================
+*/
+
+-- -----------------------------------------------------------------
+-- Analyzing sales performance over time
+-- -----------------------------------------------------------------
+
+SELECT 
+	YEAR(order_date) AS order_year,
+	SUM(sales_amount) AS total_sales,
+	AVG(sales_amount) AS avg_sales,
+	COUNT(DISTINCT customer_key) AS total_customers,
+	SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY YEAR(order_date)
+ORDER BY YEAR(order_date);
+GO
+
+-- Using DATETRUNC()
+SELECT 
+	DATETRUNC(month, order_date) AS order_year,	-- year & month
+	SUM(sales_amount) AS total_sales,
+	AVG(sales_amount) AS avg_sales,
+	COUNT(DISTINCT customer_key) AS total_customers,
+	SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(month, order_date)
+ORDER BY DATETRUNC(month, order_date);
+GO
