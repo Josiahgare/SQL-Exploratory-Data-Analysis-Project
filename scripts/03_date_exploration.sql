6420b6371c181747e8f5e57adb63791cd7d56456
Josiah Obarhuakpor <josiahgare@gmail.com>
2026-04-20 14:18:37 -0500

    Update 03_date_exploration.sql

--- scripts/03_date_exploration.sql
+++ scripts/03_date_exploration.sql
@@ -6,15 +6,21 @@
 -- Formula: MIN/MAX[Date Dimension]
 -- =====================================================
 
+-- -----------------------------------------------------------------------------------------------
 -- Find the date of the first and last order
 -- How many years of sales are available
+-- -----------------------------------------------------------------------------------------------
+
 SELECT
 	MIN(order_date) first_order_date,
 	MAX(order_date) last_order_date,
 	DATEDIFF(MONTH, MIN(order_date), MAX(order_date)) AS order_range_months
 FROM gold.fact_sales;
 
+-- -----------------------------------------------------------------------------------------------
 -- Find the youngest and the oldest customer
+-- -----------------------------------------------------------------------------------------------
+
 SELECT
 	MIN(birthdate) oldest_customer,
 	DATEDIFF(YEAR, MIN(birthdate), GETDATE()) oldest_age,
